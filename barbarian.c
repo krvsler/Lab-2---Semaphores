9cea2d80a7a871776b70b666bdd01a05ba6c02a5
krvsler <krba7261@gmail.com>
2026-05-08 03:58:59 -0700

    build more structure for barbarian.c

--- barbarian.c
+++ barbarian.c
@@ -4,18 +4,53 @@
  * Lab 2
  * barbarian.c
  * 
- * description: right now im just creating a placeholder for my character classes
+ * description: barbarian class where they attack the monster and get the treasure
 */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
 
 int main(void)
 {
+    int fd; // store the shared memory file descriptor
+    struct Dungeon *dungeon;
+
+    // open the shared memory that game.c created, also include a check
+    fd = shm_open(dungeon_shm_name, O_RDWR, 0666);
+
+    if (fd == -1)
+    {
+        print("Barbarian error with shared memory\n");
+        return 1;
+    }
+
+    // connect the shared memory to the dungeon pointer, also include a check
+    dungeon = mmap(NULL, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if (dungeon == MAP_FAILED)
+    {
+        print("Barbarian error with connecting the shared memory)\n");
+        return 1;
+    }
+
     // previews message to show that the game is running
-    printf("Barbarian starting...\n");
+    printf("Barbarian started...\n");
+
+    // stay alive when the game is running 
+    while (dungeon->running)
+    {
+        usleep(100000); // helps CPU usage
+    }
+
+    // removes barbarian mapping from shared memory
+    munmap(dungeon, sizeof(struct Dungeon));
+    close(fd);
+
+    printf("Barbarian exiting...\n");
 
     return 0;
 }
