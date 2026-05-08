accb188374b3e1a71a192ddd3b7e70b9b31626a2
krvsler <krba7261@gmail.com>
2026-05-08 00:49:46 -0700

    game plan for game.c file

--- game.c
+++ game.c
@@ -4,7 +4,7 @@
  * Lab 2
  * game.c
  * 
- * description: this is where the game will launch and run
+ * description: this is where the game will launch and run. this is the main setup file
 */
 
 #include <stdio.h>
@@ -15,6 +15,12 @@ int main(void)
 {
     // previews message to show that the game is running
     printf("Running the game...\n");
+
+    // create a shared memory named DungeonMem
+    // start the barbarian, wizard, and rogue programs
+    // save process ids for RunDungeon
+    // call RunDungeon with the process ids
+    // clean up shared memory after the dungeon is done
 
     return 0;
 }
